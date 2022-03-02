9e81cbbdc8b935987860545843bcc55233731de3
Szymon <wojdyla.szymon@gmail.com>
2022-03-02 12:07:08 +0000

    test drink has price test added

--- tests/drink_test.py
+++ tests/drink_test.py
@@ -7,3 +7,6 @@ class TestDrink(unittest.TestCase):
     
     def test_drink_has_name(self):
         self.assertEqual("red wine", self.drink.name)
+
+    def test_drink_has_price(self):
+        self.assertEqual(3.50, self.drink.price)
