8d6b206b0d65c9c96826587a46fd6e33b235876b
Szymon <wojdyla.szymon@gmail.com>
2022-03-02 11:58:43 +0000

    drinks list test added

--- tests/pub_test.py
+++ tests/pub_test.py
@@ -4,13 +4,21 @@ from src.pub import Pub
 class TestPub(unittest.TestCase):
     
     def setUp(self):
-        self.pub = Pub("The Practising Pony", 100.00) #this is set new before every single test is run
+        self.drinks = [
+            {"name": "red wine", "price": 3.50},
+            {"name": "espresso martini", "price": 5.00},
+            {"name": "gin and tonic", "price": 2.95}
+        ]
+        self.pub = Pub("The Practising Pony", 100.00, self.drinks) #this is set new before every single test is run
 
     def test_pub_has_name(self):
         self.assertEqual("The Practising Pony", self.pub.name)
 
     def test_pub_has_till(self):
         self.assertEqual(100, self.pub.till)
+
+    def test_pub_has_drinks(self):
+        self.assertEqual(self.drinks, self.pub.drinks)
 
     def test_increase_till(self):
         self.pub.increase_till(2.50)
